499ebc4b5bc7143c13fa2c902414c71084dbe25d
Francois Lesage <francois.lesage@gmail.com>
2020-06-13 00:50:26 +0200

    Added boxnbr argument / reorganised files/folders / added cloudmanager for futur use

---
+++ KISSS.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import os
+import subprocess
+import sys
+
+# Folder where folders and pictures will be created
+baseFolder = "/home/pi/Desktop/KISSS_Capture/"
+
+# Raspberry pin where you plugged the relay which manage projector remote
+relayPin = 17    # pin17
+ 
+
+def setup():
+
+      GPIO.setmode(GPIO.BCM)     
+      GPIO.setup(relayPin, GPIO.OUT)
+      GPIO.output(relayPin, GPIO.HIGH)
+
+def loop():
+
+      raw_input("Calibrate your camera and press Enter when you're ready to start the scanner...")
+
+      # Loop over a 50 sliders rack
+      for x in range(1, 51):
+            
+            GPIO.output(relayPin, GPIO.HIGH)
+            time.sleep(2)
+
+            # take picture here
+            print("Slide #%d" % (x))
+            filename = 'DSC_'+boxnbr+'_%03d.JPG'%(x,)
+            print "destination : "+destFolder+'/'+filename
+            os.system('gphoto2 --capture-image-and-download --filename='+destFolder+'/'+filename)
+
+            GPIO.output(relayPin, GPIO.LOW)
+            time.sleep(1)
+
+def destroy():
+
+      GPIO.output(relayPin, GPIO.HIGH)
+      GPIO.cleanup()                    
+
+
+# Kill the gphoto process that starts
+# whenever we turn on the camera or
+# reboot the raspberry pi
+
+def killGphoto2Process():
+    p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Search for the process we want to kill
+    for line in out.splitlines():
+        if b'gvfsd-gphoto2' in line:
+            # Kill that process!
+            pid = int(line.split(None,1)[0])
+            os.kill(pid, signal.SIGKILL)
+
+def yes_or_no(question):
+    reply = str(raw_input(question+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return yes_or_no("Uhhhh... please enter ")
+
+
+if __name__ == '__main__':     # Program start from here
+
+      # Welcome
+      print "______________________________________"
+      print "< KISSS - Keep It Simple Slide Scanner >"
+      print "--------------------------------------"
+      print "\   ^__^"
+      print "\  (oo)\_______"
+      print "   (__)\       )\/\ "
+      print "       ||----w |"
+      print "       ||     ||"
+      print " "
+
+      # Argument
+      if len(sys.argv) != 2:
+            print "Missing box number parameter"
+            quit()
+
+      boxnbr = str(sys.argv[1])
+
+      destFolder = baseFolder+'/'+boxnbr+'/'
+      # Check if dest folrder already exists
+      if not os.path.exists(destFolder):
+            os.makedirs(destFolder)
+      else:
+            if not yes_or_no(destFolder+" already exists !!! PHOTOS MAY BE OVERWRITTEN !!!\r\nAre you sure you want to continue with this directory ? "):
+                print "Exiting ..."
+                quit()
+
+      # kill gphoto instance to avoid "busy" locks 
+      killGphoto2Process()
+
+      # Setup GPIO
+      setup()
+
+      # Main loop
+      try:
+            loop()
+      except KeyboardInterrupt:  # Arret 'Ctrl+C'
+            destroy()
+
+      destroy()
--- post-processing.py
+++ post-processing.py
@@ -1,28 +1,157 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import cv2
-import imutils
+import cv2 as cv
 import numpy as np
-
-# Read the image, convert it into grayscale, and make in binary image for threshold value of 1.
-img = cv2.imread('/home/pi/projects/KISSS/images/before_crop.jpg')
-resized = imutils.resize(img, width=600)
-ratio = img.shape[0] / float(resized.shape[0])
-gray = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
-_,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
-
-# show the output image
-cv2.imshow("Image", thresh)
-cv2.waitKey(0)
-
-# Now find contours in it. There will be only one object, so find bounding rectangle for it.
-contours,hierarchy,_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-#_,contours,_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+from os import walk
 
 
-cnt = contours[0]
-x,y,w,h = cv2.boundingRect(cnt)
+def detect_left_x_mid(treeshold, myimg):
+    left = 0
+    h,w,c = myimg.shape
+    mid_h = int(h/2)
+    # Detecting first photo pixel from the middle to avoid the circle light
+    for i in range(0, w):
+        color = myimg[(mid_h,i)]
+        avg = np.mean(myimg[(mid_h,i)])
+        if avg >= treeshold :
+            if left == 0 :
+                left = i
+                break
+            if i <= left :
+                left = i
+                    #print(str(i)+","+str(j)+"/"+str(h)+" - "+str(color)+" - "+str(avg)+ " - left : "+str(left))
+                break
+    #print("middle h left "+str(left))
+    for i in range(left, 0, -1):
+        a = False
+        for j in range(0, h):
+            color = myimg[(j,i)]
+            avg = np.mean(myimg[(j,i)])
+            if avg >= treeshold :
+                if i <= left :
+                    left = i
+                    a = True
+                    break
+        if a == False:
+            break
+    #print(left)
+    return left
+def detect_right_x_mid(treeshold, myimg):
+    right = 0
+    h,w,c = myimg.shape
+    mid_h = int(h/2)
+    # Detecting first photo pixel from the middle to avoid the circle light
+    for i in range(w-1, -1, -1):
+        color = myimg[(mid_h,i)]
+        avg = np.mean(myimg[(mid_h,i)])
+        if avg >= treeshold :
+            if right == 0 :
+                right = i
+                #print(str(i)+","+str(mid_h)+"/"+str(h)+" - "+str(color)+" - "+str(avg)+ " - right : "+str(right))
+                break
+            if i >= right :
+                right = i
+                #print(str(i)+","+str(mid_h)+"/"+str(h)+" - "+str(color)+" - "+str(avg)+ " - right : "+str(right))
+                break
+    #print("middle h right "+str(right))
+    for i in range(right, w):
+        a = False
+        for j in range(0, h):
+            color = myimg[(j,i)]
+            avg = np.mean(myimg[(j,i)])
+            if avg >= treeshold :
+                if i >= right :
+                    right = i
+                    a = True
+                    break
+        if a == False:
+            break
+    #print(right)
 
-# Now crop the image, and save it into another file.
-crop = img[y:y+h,x:x+w]
-cv2.imwrite('/home/pi/projects/KISSS/images/after_crop.jpg',crop)
+    return right
+def detect_top_y_mid(treeshold, myimg, left, right):
+    top = 0
+    h,w,c = myimg.shape
+    mid_w = int(w/2)
+    for i in range(0, h):
+        color = myimg[(i,mid_w)]
+        avg = np.mean(myimg[(i,mid_w)])
+        if avg >= treeshold :
+            top = i
+            #print(str(i)+","+str(mid_h)+"/"+str(h)+" - "+str(color)+" - "+str(avg)+ " - right : "+str(right))
+            break
+    #print("middle w top "+str(top))
+    for i in range(top, -1, -1):
+        a = False
+        for j in range(left, right+1):
+            color = myimg[(i,j)]
+            avg = np.mean(myimg[(i,j)])
+            if avg >= treeshold :
+                if j <= top :
+                    top = j
+                    a = True
+                    break
+        if a == False:
+            break
+    #print(top)
+    return top
+def detect_bottom_y_mid(treeshold, myimg, left, right):
+    bottom = 0
+    h,w,c = myimg.shape
+    mid_w = int(w/2)
+
+    for i in range(h-1, -1, -1):
+        color = myimg[(i,mid_w)]
+        avg = np.mean(myimg[(i,mid_w)])
+        if avg >= treeshold :
+            bottom = i
+            #print(str(i)+","+str(mid_h)+"/"+str(h)+" - "+str(color)+" - "+str(avg)+ " - right : "+str(right))
+            break
+    #print("middle w bottom "+str(bottom))
+
+    for i in range(bottom, h):
+        a = False
+        for j in range(left, right+1):
+            color = myimg[(i,j)]
+            avg = np.mean(myimg[(i,j)])
+            if avg >= treeshold :
+                if i >= bottom :
+                    bottom = i
+                    a = True
+                    break
+        if a == False:
+            break
+    #print(bottom)
+    return bottom
+
+
+def cropper(img_source, img, lx, rx, ty, by):
+    crop_img=img[ty:by, lx:rx]
+    cv.imwrite("/home/pi/Desktop/KISSS_Capture/002/cropped/"+img_source,crop_img)
+
+def getImages():
+    f = []
+    for (dirpath, dirnames, filenames) in walk("/home/pi/Desktop/KISSS_Capture/002/"):
+        for file in filenames :
+            f.append("/home/pi/Desktop/KISSS_Capture/002/"+file)
+        #f.extend(filenames)
+        break
+    return f
+
+
+#images = ["DSC_001_7.jpg","DSC_002_3.jpg","DSC_002_4.jpg","DSC_002_6.jpg","DSC_002_19.jpg"]
+print("Cropping")
+treeshold = 55
+images = getImages()
+
+j=1
+for i in images:
+    print(str(j)+"/"+str(len(images))+"  picture : "+i)
+    myimg = cv.imread(i)
+    left_x = detect_left_x_mid(treeshold, myimg)
+    right_x = detect_right_x_mid(treeshold, myimg)
+    top_y = detect_top_y_mid(treeshold, myimg, left_x, right_x)
+    bottom_y = detect_bottom_y_mid(treeshold, myimg, left_x, right_x)
+    print(i.split("/")[6])
+    cropper(i.split("/")[6], myimg, left_x, right_x,top_y, bottom_y)
+    j+=1
